be60093bf719b72d6fee710d0b1b2edf12315c7e
BinZ <sikatunar@gmail.com>
2023-10-14 00:40:59 +0800

    Update Swap.tsx

--- apps/app-refactor/views/Swap.tsx
+++ apps/app-refactor/views/Swap.tsx
@@ -21,7 +21,17 @@ const Swap = () => {
 
   return (
     <>
-      {showSwap ? (
+      <div className="flex justify-center items-center w-full">
+        <GuideMeButton />
+        <div className="relative flex justify-center items-center sm:pb-40 py-24 sm:mt-8 sm:py-32 px-4 xl:px-0 max-w-[90rem] grow">
+          <div className="flex flex-col justify-center items-center gap-0 sm:gap-4">
+            <NotificationSwap />
+            <SwapButton />
+          </div>
+          <GuideSwap />
+        </div>
+      </div>
+      {/* {showSwap ? (
         <div className="flex justify-center items-center w-full">
           <GuideMeButton />
           <div className="relative flex justify-center items-center sm:pb-40 py-24 sm:mt-8 sm:py-32 px-4 xl:px-0 max-w-[90rem] grow">
@@ -34,7 +44,7 @@ const Swap = () => {
         </div>
       ) : (
         <NoWalletExtension />
-      )}
+      )} */}
     </>
   );
 };
