fc28c43d0125129c1f404cefbbb87346ea405c75
BinZ <sikatunar@gmail.com>
2023-11-24 22:02:58 +0800

    feat: update ABI

--- apps/app-refactor/lib/abi/index.ts
+++ apps/app-refactor/lib/abi/index.ts
@@ -3,9 +3,11 @@ import Token from "@/lib/abi/Token.json";
 import Factory from "@/lib/abi/Factory.json";
 import RouterV2 from "@/lib/abi/RouterV2.json";
 import LPToken from "@/lib/abi/LPToken.json";
+import QuickRavenSwap from "@/lib/abi/QuickRavenSwap.json";
 
 export const ButterSwapABI = ButterSwap.abi;
 export const TokenABI = Token.abi;
-export const FactoryABI = Factory;
-export const RouterV2ABI = RouterV2;
+export const FactoryABI = Factory.abi;
+export const RouterV2ABI = RouterV2.abi;
 export const LPTokenABI = LPToken.abi;
+export const QuickRavenSwapABI = QuickRavenSwap.abi;
